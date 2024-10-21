a69bef9af40bdbe1851d373c1f26e19ff5d1dc57
PauAlbu <pau.alvarez02@estudiant.upf.edu>
2024-10-21 20:19:59 +0200

    Primer intent

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,43 @@
-## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
-## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
-## COMMAND LINE SPECIFIED BELOW
-def main() :
-	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
+import sys
+
+def main():
+    x = int(sys.argv[1])
+    
+    def contador(num):
+        divis = 0
+        for i in range(1, num):
+            if num % i == 0:
+                divis = divis + 1
+        return divis
+
+    d1 = contador(x)
+
+    for i in rane(1,x)
+        if contador(i) >= d1:
+            return "not anti-prime"
+
+    return "anti-prime"
+
+# DO NOT REMOVE THIS LINE BELOW
+if __name__ == "__main__":
+    print(main())
 
 
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
 
-## DO NOT REMOVE THIS LINE BELOW
-if __name__ == "__main__" :
 
-	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
-	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
-	## $ python antiprime.py 6
-	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
-	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
-	print(main())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
